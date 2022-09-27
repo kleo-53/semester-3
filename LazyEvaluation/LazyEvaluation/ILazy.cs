0f40cd810c459eed30bec6632120ad29823bd5a4
kleo-53 <liza.1610@mail.ru>
2022-09-27 20:34:39 +0400

    realize interface and tests for lazy evaluation

---
+++ LazyEvaluation/LazyEvaluation/ILazy.cs
@@ -0,0 +1,13 @@
+﻿namespace LazyEvaluation;
+
+/// <summary>
+/// Interface for lazy evaluation
+/// </summary>
+public interface ILazy<T>
+{
+    /// <summary>
+    /// Lazy evaluation
+    /// </summary>
+    /// <returns>Result of the calculation</returns>
+    T? Get();
+}
---
+++ LazyEvaluation/LazyEvaluation/Lazy.cs
@@ -0,0 +1,41 @@
+﻿namespace LazyEvaluation;
+
+using System;
+
+/// <summary>
+/// Class which make calculation without a synchronization
+/// </summary>
+public class Lazy<T> : ILazy<T>
+{
+    /// <summary>
+    /// Result of calculation
+    /// </summary>
+    private T? result;
+
+    /// <summary>
+    /// Givan function
+    /// </summary>
+    private Func<T?> supplier;
+
+    /// <summary>
+    /// Checker of calculation
+    /// </summary>
+    private bool wasCalculated = false;
+
+    /// <summary>
+    /// Constructor of class
+    /// </summary>
+    /// <param name="supplier">Constructor of function</param>
+    public Lazy(Func<T> supplier) => this.supplier = supplier;
+
+    public T? Get()
+    {
+        if (wasCalculated)
+        {
+            return result;
+        }
+        result = supplier();
+        wasCalculated = true;
+        return result;
+    }
+}
