21c7271f7d8ca15030f155b2718d2c775634037e
kleo-53 <liza.1610@mail.ru>
2022-10-26 13:24:06 +0400

    Obtaining public information about the user

---
+++ practice2/AbobusPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AbobusPractice;
+
+using Newtonsoft.Json;
+using System;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+
+        var userID = "";
+        var serviceToken = "";
+        var field = "bdate";
+        var request = $"https://api.vk.com/method/users.get?user_id={userID}&access_token={serviceToken}&v=5.131&fields={field}";
+
+        var response = await httpClient.GetAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            var data = JsonConvert.DeserializeObject<UserInfoGetResponce>(content);
+            Console.WriteLine(data.response.First().Bdate);
+            Console.WriteLine(data.response.First().FirstName);
+        }
+    }
+
+}
---
+++ practice2/AbobusPractice/UserInfoGetResponce.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AbobusPractice;
+
+public class UserInfoGetResponce
+{
+    public List<UserInfo> response { get; set; } = new List<UserInfo>();
+
+    public class UserInfo
+    {
+        public string id { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("bdate")]
+        public string Bdate { get; set; }
+
+    }
+}
