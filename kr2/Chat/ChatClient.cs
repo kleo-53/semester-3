ed9635bd57aadfed5d0a0cbd35d585fb4a0f0512
kleo-53 <liza.1610@mail.ru>
2022-12-14 13:56:29 +0400

    chat program

---
+++ kr2/Chat/ChatClient.cs
@@ -0,0 +1,99 @@
+﻿namespace Chat;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// Class of the chat client
+/// </summary>
+public class ChatClient
+{
+    private TcpClient tcpclient;
+    private NetworkStream stream;
+    private StreamReader streamReader;
+    private StreamWriter streamWriter;
+
+    /// <summary>
+    /// Constructor of the client by IP address and port
+    /// </summary>
+    /// <param name="address">Given address</param>
+    /// <param name="port">Given port</param>
+    public ChatClient(IPAddress address, int port)
+    {
+        tcpclient = new TcpClient(address.ToString(), port);
+        stream = tcpclient.GetStream();
+        streamWriter = new StreamWriter(stream);
+        streamReader = new StreamReader(stream);
+    }
+
+    /// <summary>
+    /// Stsrts the client
+    /// </summary>
+    public async Task Run(CancellationToken cancellationToken = default)
+    {
+        using (cancellationToken.Register(() => Close()))
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var line = Console.ReadLine();
+                if (!Equals(line, ""))
+                    {
+                    await streamWriter.WriteLineAsync(line);
+
+                    await streamWriter.FlushAsync();
+                    if (Equals(line, "exit"))
+                    {
+                        Close();
+                    }
+                }
+                else
+                {
+                    var line2 = await streamReader.ReadToEndAsync();
+                    if (Equals(line2, "exit"))
+                    {
+                        Close();
+                    }
+                    Console.WriteLine($"Received: {line2}");
+                }
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sends the message to server
+    /// </summary>
+    public async Task SendMessage(string line)
+    {
+        await streamWriter.WriteLineAsync(line);
+        await streamWriter.FlushAsync();
+        if (Equals(line, "exit"))
+        {
+            Close();
+        }
+    }
+
+    /// <summary>
+    /// Recieves the message from server
+    /// </summary>
+    public async Task RecieveMessage()
+    {
+        //using var reader = new StreamReader(client.GetStream());
+        var line = await streamReader.ReadToEndAsync();
+        if (Equals(line, "exit"))
+        {
+            Close();
+        }
+        Console.WriteLine($"Received: {line}");
+    }
+
+    /// <summary>
+    /// Stops the client
+    /// </summary>
+    public async Task Close()
+    {
+        streamReader.Close();
+        streamWriter.Close();
+        tcpclient.Close();
+    }
+}
---
+++ kr2/Chat/ChatServer.cs
@@ -0,0 +1,93 @@
+﻿namespace Chat;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// Class of server
+/// </summary>
+public class ChatServer
+{
+    private TcpListener tcplistener;
+
+    /// <summary>
+    /// Constructor of the server by and port
+    /// </summary>
+    /// <param name="address">Any address</param>
+    /// <param name="port">Given port</param>
+    public ChatServer(IPAddress address, int port)
+    {
+        tcplistener = new TcpListener(address, port);
+        tcplistener.Start();
+    }
+
+    /// <summary>
+    /// Stsrts the server
+    /// </summary>
+    public async Task Run(CancellationToken cancellationToken = default)
+    {
+        using (cancellationToken.Register(() => tcplistener.Stop()))
+        {
+            var tasks = new Queue<Task>();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var tcpClient = await tcplistener.AcceptTcpClientAsync();
+                var line = Console.ReadLine();
+                if (!Equals(line, ""))
+                {
+                    using var streamWriter = new StreamWriter(tcpClient.GetStream());
+                    await streamWriter.WriteLineAsync(line);
+                    await streamWriter.FlushAsync();
+                    if (Equals(line, "exit"))
+                    {
+                        tcpClient.Close();
+                    }
+                }
+                else
+                {
+                    using var streamReader = new StreamReader(tcpClient.GetStream());
+                    var line2 = await streamReader.ReadToEndAsync();
+                    if (Equals(line2, "exit"))
+                    {
+                        tcpClient.Close();
+                    }
+                    Console.WriteLine($"Received: {line2}");
+
+                    tcpClient.Close();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sends the message to client
+    /// </summary>
+    public async Task SendMessage(TcpClient tcpClient, string line)
+    {
+        using var streamWriter = new StreamWriter(tcpClient.GetStream());
+        await streamWriter.WriteLineAsync(line);
+        await streamWriter.FlushAsync();
+        if (Equals(line, "exit"))
+        {
+            tcpClient.Close();
+        }
+    }
+    
+
+    /// <summary>
+    /// Recieves the message from client
+    /// </summary>
+    public async Task RecieveMessage(TcpClient tcpClient)
+    {
+        using var streamReader = new StreamReader(tcpClient.GetStream());
+        var line = await streamReader.ReadToEndAsync();
+        if (Equals(line, "exit"))
+        {
+            tcpClient.Close();
+        }
+        Console.WriteLine($"Received: {line}");
+
+        tcpClient.Close();
+    }
+}
