d2901f3438421eaade9eaa450eeb07ad45728c7f
kleo-53 <liza.1610@mail.ru>
2022-12-25 18:58:06 +0400

    Fix bugs

---
+++ hw2/LazyEvaluation.Tests/Tests.cs
@@ -0,0 +1,67 @@
+namespace LazyEvaluation.Tests;
+
+using NUnit.Framework;
+using System.Threading;
+
+public class Tests
+{
+    [Test]
+    public void LazyGetFunctionTest()
+    {
+        int testValue = 5;
+        var lazy = new Lazy<int>(() => testValue * testValue - 234);
+        for (int i = 0; i < 1000; ++i)
+        {
+            Assert.AreEqual(-209, lazy.Get());
+        }
+    }
+
+    [Test]
+    public void LazyThreadsGetFunctionTest()
+    {
+        int testValue = 5;
+        var lazyThreads = new LazyThreads<int>(() => testValue * testValue - 234);
+        var threads = new Thread[System.Environment.ProcessorCount];
+        for (var i = 0; i < threads.Length; ++i)
+        {
+            threads[i] = new Thread(() =>
+            {
+                Assert.AreEqual(-209, lazyThreads.Get());
+
+            });
+        }
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+
+    [Test]
+    public void BothLazyReturnNullBySupplierTest()
+    {
+        var lazy = new Lazy<string?>(() => null);
+        var lazyThreads = new LazyThreads<string?>(() => null);
+        var threads = new Thread[System.Environment.ProcessorCount];
+        for (var i = 0; i < threads.Length; ++i)
+        {
+            threads[i] = new Thread(() =>
+            {
+                Assert.IsNull(lazyThreads.Get());
+
+            });
+            Assert.IsNull(lazy.Get());
+        }
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+}
---
+++ hw2/LazyEvaluation/LazyThreads.cs
@@ -0,0 +1,53 @@
+﻿namespace LazyEvaluation;
+
+using System;
+
+/// <summary>
+/// Class which make calculation with a synchronization
+/// </summary>
+public class LazyThreads<T> : ILazy<T>
+{
+    /// <summary>
+    /// Result of calculation
+    /// </summary>
+    private T? result;
+
+    /// <summary>
+    /// Givan function
+    /// </summary>
+    private Func<T?> supplier;
+
+    /// <summary>
+    /// Checker of calculation
+    /// </summary>
+    private bool wasCalculated = false;
+
+    /// <summary>
+    /// Constructor of class
+    /// </summary>
+    /// <param name="supplier">Constructor of function</param>
+    public LazyThreads(Func<T> supplier) => this.supplier = supplier;
+
+    /// <summary>
+    /// Object to lock
+    /// </summary>
+    private object locker = new object();
+
+    public T? Get()
+    {
+        if (wasCalculated)
+        {
+            return result;
+        }
+        lock (locker)
+        {
+            if (wasCalculated)
+            {
+                return result;
+            }
+            result = supplier();
+            wasCalculated = true;
+            return result;
+        }
+    }
+}
